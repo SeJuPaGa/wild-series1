78345f9e7839567685df03aadae6177b2c7a8b7b
SEBASTIEN_VAN CLEMPUTTE <sebastienvanclemputte@gmail.com>
2024-05-30 21:03:54 +0200

    db:migrate

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+-- Création de la table 'category'
+CREATE TABLE category (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Création de la table 'program'
+CREATE TABLE program (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
